331b1f4ea8655fe5a47db2c73772b018c76e2bc0
Kent Chan <kntchn@gmail.com>
2020-01-03 19:27:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,3 +13,14 @@ end
 def map_to_no_change(array)
   array
 end
+
+def map_to_double(array)
+  num_elements = array.length
+  new_array = []
+  num_elements.times do |index|
+    new_element = array[index] * 2
+    new_array.push(new_element)
+  end
+
+  new_array
+  end
