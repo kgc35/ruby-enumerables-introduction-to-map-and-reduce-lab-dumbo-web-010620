7ebd35bcb82cbe28528e525c61698f5f4b655879
Kent Chan <kntchn@gmail.com>
2020-01-03 19:28:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,4 +23,16 @@ def map_to_double(array)
   end
 
   new_array
+end
+
+def map_to_square(array)
+  num_elements = array.length
+  new_array = []
+  num_elements.times do |index|
+    new_element = array[index] **2
+    new_array.push(new_element)
   end
+
+  new_array
+  end
+  
