e0b00efa5d32c64dd26b6fd7e0ecc6458d8fb161
Kent Chan <kntchn@gmail.com>
2020-01-03 19:26:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,11 @@
 require 'matrix'
 def map_to_negativize(array)
-  new_array = -1 * matrix[array]
-  print new_array
+num_elements = array.length
+new_array = []
+num_elements.times do |index|
+  new_element = array[index] * -1
+  new_array.push(new_element)
+end
+
+new_array
 end
