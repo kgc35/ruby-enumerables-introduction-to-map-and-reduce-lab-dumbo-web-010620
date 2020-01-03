6184a973d3579a83ed5f1ec279523724a0f164f1
Kent Chan <kntchn@gmail.com>
2020-01-03 19:31:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,3 +35,15 @@ def map_to_square(array)
 
   new_array
   end
+
+  def reduce_to_total(array, starting_point = 0)
+    num_elements = array.length
+    total_sum = 0;
+    num_elements.times do |index|
+      total_sum += array[index]
+    end
+    total_sum += starting_point
+    total_sum
+    end
+
+    new_array
